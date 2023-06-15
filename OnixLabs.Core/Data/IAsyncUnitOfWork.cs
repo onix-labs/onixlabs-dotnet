d8b4d7e45eb61893f5245785a2da596f36c34f2f
Matthew Layton <9935122+MrMatthewLayton@users.noreply.github.com>
2023-06-15 21:37:57 +0100

    Added base repository and unit-of-work interfaces. (#22)

---
+++ OnixLabs.Core/Data/IAsyncUnitOfWork.cs
@@ -0,0 +1,29 @@
+// Copyright 2020-2023 ONIXLabs
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+
+namespace OnixLabs.Core.Data;
+
+/// <summary>
+/// Represents the base contract for implementing the unit of work pattern using async/await.
+/// </summary>
+public interface IAsyncUnitOfWork
+{
+    /// <summary>
+    /// Commits a unit of work.
+    /// </summary>
+    /// <returns>Returns the number of changes that were successfully committed in the unit of work.</returns>
+    Task<int> SaveChangesAsync();
+}

911c8293b123dc9890309061b1d5ff59eb659ba5
Matthew Layton <matthew.layton@live.co.uk>
2024-01-09 22:22:35 +0000

    Added BigDecimal.SetScale tests.

--- OnixLabs.Playground/Program.cs
+++ OnixLabs.Playground/Program.cs
@@ -12,11 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using ScaleMode = OnixLabs.Numerics.ScaleMode;
+
 namespace OnixLabs.Playground;
 
 internal static class Program
 {
     private static void Main()
     {
+        decimal[] values =
+        [
+            +0.1234567890987654321000000000m,
+            +1.1234567890987654321000000000m,
+            -0.1234567890987654321000000000m,
+            -1.1234567890987654321000000000m
+        ];
+
+        int[] scales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28];
+
+        foreach (decimal value in values)
+        {
+            foreach (int scale in scales)
+            {
+                foreach (MidpointRounding rounding in Enum.GetValues<MidpointRounding>())
+                {
+                    decimal expected = decimal.Round(value, scale, rounding);
+                    Console.WriteLine($"({value}, {scale}, {rounding}, {expected})");
+                }
+            }
+        }
     }
 }

e05b96e8dab3325c9be1997cbb528c03209d45b6
Matthew Layton <matthew.layton@live.co.uk>
2024-01-27 10:50:48 +0000

    Minor code style improvements.

--- OnixLabs.Core/Text/StringBuilderExtensions.cs
+++ OnixLabs.Core/Text/StringBuilderExtensions.cs
@@ -1,11 +1,11 @@
 // Copyright © 2020 ONIXLabs
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //    http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using OnixLabs.Core.Linq;
 
@@ -34,8 +33,7 @@ public static class StringBuilderExtensions
     /// <returns>Returns the current <see cref="StringBuilder"/> with the specified items appended.</returns>
     public static StringBuilder Append(this StringBuilder builder, params object[] values)
     {
-        builder.Append(values.JoinToString(string.Empty));
-        return builder;
+        return builder.Append(values.JoinToString(string.Empty));
     }
 
     /// <summary>
@@ -46,12 +44,11 @@ public static class StringBuilderExtensions
     /// <returns>Returns the current <see cref="StringBuilder"/> with the specified items prepended.</returns>
     public static StringBuilder Prepend(this StringBuilder builder, params object[] values)
     {
-        builder.Insert(0, values.JoinToString(string.Empty));
-        return builder;
+        return builder.Insert(0, values.JoinToString(string.Empty));
     }
 
     /// <summary>
-    /// Trims the specified <see cref="char"/> value from the start and end of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="char"/> value from the start and end of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="char"/> value to trim.</param>
@@ -62,31 +59,35 @@ public static class StringBuilderExtensions
     }
 
     /// <summary>
-    /// Trims the specified <see cref="char"/> value from the end of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="char"/> value from the end of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="char"/> value to trim.</param>
     /// <returns>Returns the current <see cref="StringBuilder"/> with the specified <see cref="char"/> value trimmed from the end.</returns>
     public static StringBuilder TrimEnd(this StringBuilder builder, char value)
     {
-        while (builder.Length > 0 && builder[^1] == value) builder.Remove(builder.Length - 1, 1);
+        while (builder.Length > 0 && builder[^1] == value)
+            builder.Remove(builder.Length - 1, 1);
+
         return builder;
     }
 
     /// <summary>
-    /// Trims the specified <see cref="char"/> value from the start of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="char"/> value from the start of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="char"/> value to trim.</param>
     /// <returns>Returns the current <see cref="StringBuilder"/> with the specified <see cref="char"/> value trimmed from the start.</returns>
     public static StringBuilder TrimStart(this StringBuilder builder, char value)
     {
-        while (builder.Length > 0 && builder[0] == value) builder.Remove(0, 1);
+        while (builder.Length > 0 && builder[0] == value)
+            builder.Remove(0, 1);
+
         return builder;
     }
 
     /// <summary>
-    /// Trims the specified <see cref="string"/> value from the start and end of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="string"/> value from the start and end of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="string"/> value to trim.</param>
@@ -97,7 +98,7 @@ public static class StringBuilderExtensions
     }
 
     /// <summary>
-    /// Trims the specified <see cref="string"/> value from the end of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="string"/> value from the end of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="string"/> value to trim.</param>
@@ -105,15 +106,13 @@ public static class StringBuilderExtensions
     public static StringBuilder TrimEnd(this StringBuilder builder, string value)
     {
         while (builder.Length >= value.Length && builder.ToString(builder.Length - value.Length, value.Length) == value)
-        {
             builder.Remove(builder.Length - value.Length, value.Length);
-        }
 
         return builder;
     }
 
     /// <summary>
-    /// Trims the specified <see cref="string"/> value from the start of the current <see cref="StringBuilder"/>. 
+    /// Trims the specified <see cref="string"/> value from the start of the current <see cref="StringBuilder"/>.
     /// </summary>
     /// <param name="builder">The <see cref="StringBuilder"/> to trim.</param>
     /// <param name="value">The <see cref="string"/> value to trim.</param>
@@ -121,9 +120,7 @@ public static class StringBuilderExtensions
     public static StringBuilder TrimStart(this StringBuilder builder, string value)
     {
         while (builder.Length >= value.Length && builder.ToString(0, value.Length) == value)
-        {
             builder.Remove(0, value.Length);
-        }
 
         return builder;
     }
@@ -137,9 +134,7 @@ public static class StringBuilderExtensions
     /// <returns>Returns the current <see cref="StringBuilder"/> wrapped between the specified start and end <see cref="char"/> values.</returns>
     public static StringBuilder Wrap(this StringBuilder builder, char start, char end)
     {
-        builder.Prepend(start);
-        builder.Append(end);
-        return builder;
+        return builder.Prepend(start).Append(end);
     }
 
     /// <summary>
@@ -151,8 +146,6 @@ public static class StringBuilderExtensions
     /// <returns>Returns the current <see cref="StringBuilder"/> wrapped between the specified start and end <see cref="string"/> values.</returns>
     public static StringBuilder Wrap(this StringBuilder builder, string start, string end)
     {
-        builder.Prepend(start);
-        builder.Append(end);
-        return builder;
+        return builder.Prepend(start).Append(end);
     }
 }

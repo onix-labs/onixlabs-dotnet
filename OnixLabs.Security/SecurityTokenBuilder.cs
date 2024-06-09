8094b158d27f6350d1555c3b957f804e4da0dee2
Matthew Layton <matthew.layton@live.co.uk>
2024-06-09 20:32:54 +0100

    Nuget package updates.

--- OnixLabs.Security/SecurityTokenBuilder.cs
+++ OnixLabs.Security/SecurityTokenBuilder.cs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace OnixLabs.Security;
 
 /// <summary>

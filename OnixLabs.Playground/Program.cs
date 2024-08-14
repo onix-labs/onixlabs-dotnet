1a4ab153ed60b6c2fcfd294dfd3487d6bcb97628
Matthew Layton <matthew.layton@live.co.uk>
2024-08-14 23:52:49 +0100

    Removed example code from playground.

--- OnixLabs.Playground/Program.cs
+++ OnixLabs.Playground/Program.cs
@@ -12,24 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using OnixLabs.Core;
-
 namespace OnixLabs.Playground;
 
 internal static class Program
 {
     private static void Main()
     {
-        string[] names = ["Alice", "Bob", "Charlie", "Dave"];
-        Random random = new();
-
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
-        Console.WriteLine(random.Next(names));
     }
 }

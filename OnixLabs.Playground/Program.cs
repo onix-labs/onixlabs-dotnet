7c1baa98e2d3edfb96bcb9045f5c27dbabd9056f
Matthew Layton <matthew.layton@live.co.uk>
2024-08-18 23:07:10 +0100

    Removed playground code.

--- OnixLabs.Playground/Program.cs
+++ OnixLabs.Playground/Program.cs
@@ -12,22 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using OnixLabs.Core;
-
 namespace OnixLabs.Playground;
 
 internal static class Program
 {
     private static void Main()
     {
-        Optional<int> result = GetResult().GetValueOrNone();
-        Console.WriteLine(result);
-        Console.WriteLine(Optional<int>.None == result);
-    }
-
-    private static Result<int> GetResult()
-    {
-        return 0;
     }
 }

084ae4d1269373b1908614484455f200cfb7d78b
Matthew Layton <matthew.layton@live.co.uk>
2025-12-16 13:23:01 +0000

    Updated the Core/Text implementation to include more constructors, better documentation, and fewer allocations.

--- OnixLabs.Core/Text/Base58FormatProvider.cs
+++ OnixLabs.Core/Text/Base58FormatProvider.cs
@@ -51,11 +51,6 @@ public sealed class Base58FormatProvider : Enumeration<Base58FormatProvider>, IF
     /// </summary>
     public string Alphabet { get; }
 
-    /// <summary>Gets an object that provides formatting services for the specified type.</summary>
-    /// <param name="formatType">An object that specifies the type of format object to return.</param>
-    /// <returns>
-    /// Returns an instance of the object specified by <paramref name="formatType"/>,
-    /// if the <see cref="T:IFormatProvider"/> implementation can supply that type of object; otherwise, <see langword="null"/>.
-    /// </returns>
+    /// <inheritdoc/>
     public object? GetFormat(Type? formatType) => formatType == typeof(Base58FormatProvider) ? this : null;
 }
